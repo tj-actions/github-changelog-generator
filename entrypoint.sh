7aba00296a76d2719c473fab3b5ad7057bc1135c
Tonye Jack <tonye@coherentpath.com>
2021-02-20 18:42:59 -0500

    Fixed test.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-REPOSITORY="$(cut -d '/' -f 2 <<< "$GITHUB_ACTION_REPOSITORY")"
+REPOSITORY=$(cut -d '/' -f 2 <<< "$GITHUB_ACTION_REPOSITORY")
 
 echo "Running github_changelog_generator with the following settings:"
 echo "Repository: $REPOSITORY"
 echo "Repository owner: $GITHUB_REPOSITORY_OWNER"
-echo "Output: $INPUT_OUPUT"
+echo "Output: $INPUT_OUTPUT"
 
 github_changelog_generator -u "$GITHUB_REPOSITORY_OWNER" -p "$REPOSITORY" --token "$INPUT_TOKEN" -o "$INPUT_OUPUT"
