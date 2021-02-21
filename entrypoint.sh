a2171cbad58202cdb0694ac262e898082e10b355
Tonye Jack <tonye@coherentpath.com>
2021-02-20 21:02:02 -0500

    Update script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ echo "Repository: $REPOSITORY"
 echo "Repository owner: $GITHUB_REPOSITORY_OWNER"
 echo "Output: $INPUT_OUPUT"
 
-github_changelog_generator -u "$GITHUB_REPOSITORY_OWNER" -p "$REPOSITORY" --token "$INPUT_TOKEN" -o "$INPUT_OUPUT"
+github_changelog_generator -u "$GITHUB_REPOSITORY_OWNER" -p "$REPOSITORY" -t "$INPUT_TOKEN" --base="$INPUT_OUPUT" --output="$INPUT_OUPUT"
