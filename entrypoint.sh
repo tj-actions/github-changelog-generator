d83669723aa2f606adf0570569d7451a9e722b39
Tonye Jack <tonye@coherentpath.com>
2021-02-20 20:56:13 -0500

    Update script.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ REPOSITORY=$(cut -d '/' -f 2 <<< "$GITHUB_REPOSITORY")
 echo "Running github_changelog_generator with the following settings:"
 echo "Repository: $REPOSITORY"
 echo "Repository owner: $GITHUB_REPOSITORY_OWNER"
-echo "Output: $GITHUB_WORKSPACE/$INPUT_OUPUT"
+echo "Output: $GITHUB_WORKSPACE\/$INPUT_OUPUT"
 
-github_changelog_generator -u "$GITHUB_REPOSITORY_OWNER" -p "$REPOSITORY" --token "$INPUT_TOKEN" -o "$GITHUB_WORKSPACE/$INPUT_OUPUT"
+github_changelog_generator -u "$GITHUB_REPOSITORY_OWNER" -p "$REPOSITORY" --token "$INPUT_TOKEN" -o "$GITHUB_WORKSPACE\/$INPUT_OUPUT"
