a25d78146a792ff470c785dc4650e8d32dc8fe9d
Tonye Jack <jtonye@ymail.com>
2022-02-14 21:34:32 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ REPOSITORY=$(cut -d '/' -f 2 <<< "$GITHUB_REPOSITORY")
 echo "Running github_changelog_generator with the following settings:"
 echo "Repository: $REPOSITORY"
 echo "Repository owner: $GITHUB_REPOSITORY_OWNER"
-echo "Output: $2"
+echo "Output: $INPUT_OUTPUT"
 
-github_changelog_generator -u "$GITHUB_REPOSITORY_OWNER" -p "$REPOSITORY" -t "$1" --base "" --output="$2"
+github_changelog_generator -u "$GITHUB_REPOSITORY_OWNER" -p "$REPOSITORY" -t "$INPUT_TOKEN" --base "" --output="$INPUT_OUTPUT"
